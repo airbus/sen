a082caf81192c5dd0926d9abd84d77f16bbbe036
Airbus contributor <florian.sattler@airbus.com>
2026-03-23 17:54:17 +0100

    test[core]: adds tests for meta
    Adds test for:
    * class_type
    * type_test
    * unit

--- libs/core/test/meta/type_test.cpp
+++ libs/core/test/meta/type_test.cpp
@@ -17,6 +17,9 @@
 using sen::TypeHandle;
 using sen::VoidType;
 
+/// @test
+/// Checks that makeNonOwningTypeHandle wraps a raw pointer without taking ownership
+/// @requirements(SEN-355)
 TEST(TypeHandle, createFromNonOwning)
 {
   const VoidType* type = sen::VoidType::get().type();
@@ -28,6 +31,9 @@ TEST(TypeHandle, createFromNonOwning)
   EXPECT_EQ(type, createdConstTypeHandle.type());
 }
 
+/// @test
+/// Checks implicit owning conversion from TypeHandle<Derived> to TypeHandle<Base>
+/// @requirements(SEN-355)
 TEST(TypeHandle, createFromConvertableType)
 {
   auto testAlias = sen::AliasType::make(sen::AliasSpec("Test", "test", "test description", sen::VoidType::get()));
@@ -37,6 +43,9 @@ TEST(TypeHandle, createFromConvertableType)
   EXPECT_EQ(testAlias.type(), testCustomType.type());
 }
 
+/// @test
+/// Checks implicit non-owning conversion from TypeHandle<Deriver> to TypeHandle<Base>
+/// @requirements(SEN-355)
 TEST(TypeHandle, createFromConvertableTypeNonOwning)
 {
   auto testAlias = sen::AliasType::make(sen::AliasSpec("Test", "test", "test description", sen::VoidType::get()));
@@ -47,6 +56,9 @@ TEST(TypeHandle, createFromConvertableTypeNonOwning)
   EXPECT_EQ(testAlias.type(), testCustomType.type());
 }
 
+/// @test
+/// Checks implicit owning conversion from temporary TypeHandle<Derived>
+/// @requirements(SEN-355)
 TEST(TypeHandle, createFromConvertableTypeFromTmp)
 {
   TypeHandle<sen::CustomType> testCustomType =
@@ -55,6 +67,9 @@ TEST(TypeHandle, createFromConvertableTypeFromTmp)
   EXPECT_EQ(sen::VoidType::get(), testCustomType->asAliasType()->getAliasedType());
 }
 
+/// @test
+/// Checks that non-owning handle converted to TypeHandle<Base> preserves the raw pointer
+/// @requirements(SEN-355)
 TEST(TypeHandle, createFromConvertableTypeNonOwningFromTmp)
 {
   auto testAlias = sen::AliasType::make(sen::AliasSpec("Test", "test", "test description", sen::VoidType::get()));
@@ -64,6 +79,9 @@ TEST(TypeHandle, createFromConvertableTypeNonOwningFromTmp)
   EXPECT_EQ(testAlias.type(), testCustomType.type());
 }
 
+/// @test
+/// Verifies operator== and operator!=
+/// @requirements(SEN-355)
 TEST(TypeHandle, testComparisions)
 {
   const VoidType* type = sen::VoidType::get().type();
@@ -109,6 +127,9 @@ TEST(TypeHandle, testComparisions)
   EXPECT_TRUE(*otherTypeHandle.type() != createdConstTypeHandle);
 }
 
+/// @test
+/// Verifies type(), operator->() and operator*()
+/// @requirements(SEN-355)
 TEST(TypeHandle, testAccessCapabilities)
 {
   const VoidType* type = sen::VoidType::get().type();
@@ -125,6 +146,9 @@ TEST(TypeHandle, testAccessCapabilities)
   EXPECT_EQ(*type, *createdConstTypeHandle);
 }
 
+/// @test
+/// Checks dynamicTypeHandleCast succeeds when the managed type is convertible to the target
+/// @requirements(SEN-355)
 TEST(DynamicTypeHandleCast, correctUpwardsCast)
 {
   auto testAlias = sen::AliasType::make(sen::AliasSpec("Test", "test", "test description", sen::VoidType::get()));
@@ -137,6 +161,9 @@ TEST(DynamicTypeHandleCast, correctUpwardsCast)
   EXPECT_EQ(testAlias, maybeUpcastedAliasType.value());
 }
 
+/// @test
+/// Checks dynamicTypeHandleCast returns nullopt when the managed type is not convertible
+/// @requirements(SEN-355)
 TEST(DynamicTypeHandleCast, notallowedUpwardsCast)
 {
   auto testAlias = sen::AliasType::make(sen::AliasSpec("Test", "test", "test description", sen::VoidType::get()));
@@ -146,4 +173,126 @@ TEST(DynamicTypeHandleCast, notallowedUpwardsCast)
   auto maybeUpcastedVoidType = sen::dynamicTypeHandleCast<sen::VoidType>(testCustomType);
 
   EXPECT_FALSE(maybeUpcastedVoidType.has_value());
+}
+
+/// @test
+/// Checks all validation branches of validateLowerCaseName
+/// @requirements(SEN-355)
+TEST(Type, validateLowerCaseName)
+{
+  // valid name
+  {
+    auto result = sen::Type::validateLowerCaseName("validName123");
+    EXPECT_TRUE(result.isOk());
+  }
+
+  // valid name with dot separator
+  {
+    auto result = sen::Type::validateLowerCaseName("some.name");
+    EXPECT_TRUE(result.isOk());
+  }
+
+  // empty name
+  {
+    auto result = sen::Type::validateLowerCaseName("");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "empty name");
+  }
+
+  // starts with digit
+  {
+    auto result = sen::Type::validateLowerCaseName("1invalid");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "not allowed to start with digits");
+  }
+
+  // starts with uppercase letter
+  {
+    auto result = sen::Type::validateLowerCaseName("Invalid");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "not allowed to start with uppercase letter");
+  }
+
+  // starts with underscore
+  {
+    auto result = sen::Type::validateLowerCaseName("_invalid");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "not allowed to start with _");
+  }
+
+  // starts with dot
+  {
+    auto result = sen::Type::validateLowerCaseName(".invalid");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "not allowed to start with .");
+  }
+
+  // contains invalid character
+  {
+    auto result = sen::Type::validateLowerCaseName("hello!world");
+    EXPECT_TRUE(result.isError());
+    EXPECT_NE(result.getError().find("name must only contain alpha-numeric characters"), std::string::npos);
+    EXPECT_NE(result.getError().find("!"), std::string::npos);
+  }
+}
+
+/// @test
+/// Checks all validation branches of validateTypeName
+/// @requirements(SEN-355)
+TEST(Type, validateTypeName)
+{
+  // valid name
+  {
+    auto result = sen::Type::validateTypeName("ValidName123");
+    EXPECT_TRUE(result.isOk());
+  }
+
+  // valid name with dot separator
+  {
+    auto result = sen::Type::validateTypeName("Some.Qualified.Name");
+    EXPECT_TRUE(result.isOk());
+  }
+
+  // empty name
+  {
+    auto result = sen::Type::validateTypeName("");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "empty name");
+  }
+
+  // starts with digit
+  {
+    auto result = sen::Type::validateTypeName("1Invalid");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "not allowed to start with digits");
+  }
+
+  // starts with lowercase letter
+  {
+    auto result = sen::Type::validateTypeName("invalid");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "not allowed to start with lowercase letter");
+  }
+
+  // starts with underscore
+  {
+    auto result = sen::Type::validateTypeName("_Invalid");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "not allowed to start with _");
+  }
+
+  // starts with dot
+  {
+    auto result = sen::Type::validateTypeName(".Invalid");
+    EXPECT_TRUE(result.isError());
+    EXPECT_EQ(result.getError(), "not allowed to start with .");
+  }
+
+  // contains invalid character
+  {
+    auto result = sen::Type::validateTypeName("Hello!World");
+    EXPECT_TRUE(result.isError());
+    EXPECT_NE(result.getError().find("name must only contain alpha-numeric characters"), std::string::npos);
+    EXPECT_NE(result.getError().find("!"), std::string::npos);
+  }
 }
